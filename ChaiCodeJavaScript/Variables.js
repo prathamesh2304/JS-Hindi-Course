4150fe8044e4305fec28428f1e25a2055759944e
prathamesh2304 <prathameshthakur.official@gmail.com>
2024-10-10 11:39:13 +0000

    variables

---
+++ ChaiCodeJavaScript/Variables.js
@@ -0,0 +1,7 @@
+const accountNumber = 12345
+var accountName = "Prathamesh"
+let accountEmail = "prathameshthakur9421@gmail.com"
+accountCity = "Chh sambhajinagar"
+
+accountNumber = 4321
+console.log(accountNumber);
